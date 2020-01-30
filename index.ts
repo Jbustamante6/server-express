cba670b13d4553d5b2233b47424305af9095673f
Jefersson Bustamante <jefersson.bustamante@ideaware.co>
2020-01-30 17:20:10 -0500

    first commit

---
+++ index.ts
@@ -0,0 +1,13 @@
+import Server from './classes/server';
+import router from './routes/routes';
+import bodyParser from 'body-parser';
+const server = new Server();
+
+
+server.app.use(bodyParser.urlencoded({extended: true}))
+
+server.app.use('/', router);
+
+server.start( () =>{ 
+    console.log(server.port);
+});
