2bd958fd80cd92cfb199373597e29d5040dcd24b
Jefersson Bustamante <jefersson.bustamante@ideaware.co>
2020-01-30 17:45:12 -0500

    finalizacion construccion server

--- routes/routes.ts
+++ routes/routes.ts
@@ -11,10 +11,25 @@ router.get('/mensajes', (req: Request, res: Response) =>{
 });
 
 router.post('/mensajes', (req: Request, res: Response) =>{
+
+    const mensaje = req.body.mensaje;
+
     res.json({
        ok: true,
-       mensaje: 'POSTEADO' 
+       mensaje 
     });
 });
+
+router.post('/mensajes/:id', (req: Request, res: Response) =>{
+
+    const mensaje = req.body.mensaje;
+    const id = req.params.id;
+    res.json({
+       ok: true,
+       mensaje,
+       id
+    });
+});
+
 
 export default router;
