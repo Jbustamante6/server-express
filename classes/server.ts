b50979f053eb279cde9650aae4421bc116180196
Jefersson Bustamante <jefersson.bustamante@ideaware.co>
2020-02-27 17:57:21 -0500

    envio de mensajes

--- classes/server.ts
+++ classes/server.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import { SERVER_PORT } from '../global/environment';
 import socketIO from 'socket.io';
 import http from 'http';
-
+import * as socket from  '../sockets/sockets';
 export default class Server {
     
     private static _instance: Server;
@@ -29,9 +29,9 @@ export default class Server {
         this.io.on('connection', cliente =>{
             console.log('Nuevo cliente conectado');
 
-            cliente.on('disconnect', () =>{
-                console.log('Cliente desconectado')
-            })
+            socket.mensaje(cliente);
+
+            socket.desconectar(cliente);
         });
     }
 
---
+++ sockets/sockets.ts
@@ -0,0 +1,13 @@
+import { Socket } from "socket.io";
+
+export const desconectar = (cliente: Socket) => {
+    cliente.on('disconect', () =>{
+        console.log('desconectar');
+    });
+}
+
+export const mensaje = (cliente: Socket) =>{
+    cliente.on('mensaje', ( payload:{de: string, cuerpo: string} )=>{
+        console.log('Mensaje recibido', payload)
+    });
+}
